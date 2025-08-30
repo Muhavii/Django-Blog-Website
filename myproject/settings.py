22f35335416fb2845ea6b94bb383a122a5bc41b9
Muhavii <griffinsmuhavi@gmail.com>
2025-08-30 13:34:13 +0300

    Hardcode SECRET_KEY to fix CSRF issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
+SECRET_KEY = '03a1lv%#05qkdg(33+gig3hi=*z3pzoly95t()*5c)x7bawwo8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
