37d8a5c9126b66190ee923a3f97be4d013e2f0e5
Muhavii <griffinsmuhavi@gmail.com>
2025-08-30 14:32:49 +0300

    Turn off DEBUG for production

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-simple-key-for-production-123456789'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
